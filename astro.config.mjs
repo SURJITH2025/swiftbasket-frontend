318c7d809c82ddcf37c716dda04322073a7f22f4
SURJITH JAYAKUMAR <jksurjith@gmail.com>
2026-01-17 14:42:28 +0530

    Change allowedHosts to 'all' in Vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,7 @@ export default defineConfig({
     server: {
       host: true,
       port: 4321,
-      allowedHosts: [
-        ".csb.app",
-        "localhost"
-      ]
+      allowedHosts: "all"
     }
   }
 });
